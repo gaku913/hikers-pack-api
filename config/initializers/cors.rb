8614ebbf00aa4ed40d2a7e7ea7a417e1996d4f11
Gaku Ito <gaku.i.913@gmail.com>
2024-01-30 03:07:00 +0000

    Add User authentication

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,3 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
@@ -11,6 +6,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource "*",
       headers: :any,
+      expose: %w[access-token uid client],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
